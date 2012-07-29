8b56ed4d62d81be7c80a3d2477dbd2e1e54272d1
somithashedin <somit@hashedin.com>
2012-07-29 17:39:46 +0530

    Image Controller Made

--- src/main/java/com/hashedin/flicky/web/Album.java
+++ src/main/java/com/hashedin/flicky/web/Album.java
@@ -1,32 +1,33 @@
 package com.hashedin.flicky.web;
 
-import java.util.Date;
+import java.util.List;
 
 public class Album {
-
-	private String ID;
+	private String uid;
 	private String name;
 	private String description;
-	private Date creationDate;
-	
-	/**
-	 * @return the iD
-	 */
-	public String getID() {
-		return ID;
-	}
-	/**
-	 * @param iD the iD to set
-	 */
-	public void setID(String iD) {
-		ID = iD;
-	}
-	
+	private String creationDate;
+	private List<Image> listOfImages;
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
+		
 		return name;
+		
+	}
+	/**
+	 * @return the uid
+	 */
+	public String getUid() {
+		return uid;
+	}
+	/**
+	 * @param uid the uid to set
+	 */
+	public void setUid(String uid) {
+		this.uid = uid;
 	}
 	/**
 	 * @param name the name to set
@@ -49,14 +50,28 @@ public class Album {
 	/**
 	 * @return the creationDate
 	 */
-	public Date getCreationDate() {
+	public String getCreationDate() {
 		return creationDate;
 	}
 	/**
 	 * @param creationDate the creationDate to set
 	 */
-	public void setCreationDate(Date creationDate) {
+	public void setCreationDate(String creationDate) {
 		this.creationDate = creationDate;
 	}
-		
+	/**
+	 * @return the listOfImages
+	 */
+	public List<Image> getListOfImages() {
+		return listOfImages;
+	}
+	/**
+	 * @param listOfImages the listOfImages to set
+	 */
+	public void setListOfImages(List<Image> listOfImages) {
+		this.listOfImages = listOfImages;
+	}
+	public void addImagetoList(Image image){
+		this.listOfImages.add(image);
+	}
 }
--- src/main/java/com/hashedin/flicky/web/AlbumsController.java
+++ src/main/java/com/hashedin/flicky/web/AlbumsController.java
@@ -1,32 +1,99 @@
 package com.hashedin.flicky.web;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.hashedin.flicky.manager.AlbumManager;
 
 @Controller
 public class AlbumsController {
+	@Autowired
+	private AlbumManager db;
 
-	//private Database db;
-	@RequestMapping("/albums/{id}")	
-	public ModelAndView albums(String id) {
-		Album album = getAlbum(id);
+	@RequestMapping("/albums/{uid}")	
+	public ModelAndView albums(@PathVariable String uid){
+		//Map<String, Album> album = db.getAlbums();
+
+	//		List<Image> listImages= album.getListOfImages();
+//		Image image = new Image();
+//		image.setDate("2012-2-3");
+//		image.setId(1);
+//		image.setName("1.jpg");
+//		albumSearch.addImagetoList(image);
+//		
+//		image = new Image();
+//		image.setDate("2012-2-4");
+//		image.setId(2);
+//		image.setName("2.jpg");
+//		albumSearch.addImagetoList(image);
+//		
+//		image = new Image();
+//		image.setDate("2012-2-3");
+//		image.setId(3);
+//		image.setName("3.jpg");
+//		albumSearch.addImagetoList(image);
+//	
+//		
+//		image = new Image();
+//		image.setDate("2012-2-3");
+//		image.setId(4);
+//		image.setName("4.jpg");
+//		albumSearch.addImagetoList(image);
+//		
+//		image = new Image();
+//		image.setDate("2012-2-3");
+//		image.setId(5);
+//		image.setName("5.jpg");
+//		albumSearch.addImagetoList(image);
+
+//		List<Integer> listImages=new ArrayList<Integer>();
+//		listImages.add(1);
+//		listImages.add(2);
+//		listImages.add(3);
+//		listImages.add(4);
+//		listImages.add(5);
+//		listImages.add(6);
+		//String[] listOfImages= {"1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"};
+		//List<Image> listOfImages= new ArrayList<Image>();
+		//listOfImages=album.getListOfImages();
+		System.out.println(uid);
+		Album album=db.getTheAlbum(uid);
 		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("album", album);
-		
+	      model.put("albumImages", album.getListOfImages());
 		ModelAndView modelAndView = new ModelAndView("albums", model);
 		return modelAndView;
 	}
-
-	private Album getAlbum(String id) {
-		// TODO Read from Database
-		Album a = new Album();
-		a.setID(id);
-		a.setName("Leh Photos");
-		a.setDescription("Photos shot during my Leh visit in 2010");
-		return a;
-	}
+	
+    @RequestMapping(value="/albums", method = RequestMethod.GET)
+   public ModelAndView add (String name,String description) {
+    	Map<String, Object> model = new HashMap<String, Object>();
+		model.put("albumImages", null);
+		ModelAndView modelAndView = new ModelAndView("upload", model);
+		return modelAndView;
+//    	System.out.println("asdasd");
+//    	System.out.println(name+"   "+description);
+////        if (result.hasErrors()) {
+////            return "/albums
+////        }
+////       // appointmentBook.addAppointment(appointment);
+////        return "redirect:/albums/";
+//    	Map<String, Object> model = new HashMap<String, Object>();
+//		model.put("albumImages", null);
+//		ModelAndView modelAndView = new ModelAndView("upload", model);
+//		return modelAndView;
+//    }
+    }
 }
+	
+
+
